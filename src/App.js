57a66cc20555b38109519f5e9e009f742ac4ef9a
Minwoo02 <onyx9250@naver.com>
2023-09-19 05:54:50 +0900

    Animation & Pseudo Selectors

--- src/App.js
+++ src/App.js
@@ -1,21 +1,55 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Father = styled.div`
   display: flex;
 `;
 
-const Input = styled.input.attrs({ required: true, minLength: 10 })`
+const rotateAnimation = keyframes`
+  0% {
+    transform:rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    transform: rotate(360deg);
+    border-radius: 100px;
+  }
+  100% {
+    transform:rotate(360deg);
+    border-radius: 0px;
+  }
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
   background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotateAnimation} 2s linear infinite;
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 40px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
+  /* span: hover {
+    font-size: 40px;
+  }
+  span: active {
+    opacity: 0;
+  } */
 `;
 
 function App() {
   return (
     <Father>
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
+      <Box>
+        <span>😁</span>
+      </Box>
     </Father>
   );
 }
