fb564edbd1368d706c3f79c7ceaafe74fa950aac
Minwoo02 <onyx9250@naver.com>
2023-09-24 21:36:26 +0900

    Coin Data (Crypto Tracker)

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -37,12 +37,26 @@ function Coin() {
   const [loading, setLoading] = useState(true);
   const { coinId } = useParams<RouteParams>();
   const { state } = useLocation<RouteState>();
+  const [info, setInfo] = useState({});
+  const [priceInfo, setPriceInfo] = useState({});
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      setInfo(infoData);
+      setPriceInfo(priceData);
+    })();
+  }, []);
   return (
     <Container>
       <Header>
         <Title>{state?.name || "Loading.."}</Title>
       </Header>
-      {loading ? "Loading..." : null}
+      {loading ? <Loader>"Loading..."</Loader> : null}
     </Container>
   );
 }
